888a06ca338c0ef8405172a329ac1738f4ab3d64
yacine benaffane <yacine.benaffane@gmail.com>
2022-11-28 22:25:53 +0100

    change title of page

--- custom-init.js
+++ custom-init.js
@@ -5,7 +5,7 @@ class CustomInit extends HTMLElement {
         <meta charset="utf-8">
 <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-<title>PhotoFolio Bootstrap Template - Index</title>
+<title>Yacine Benaffane Portofolio Photographe</title>
 <meta content="" name="description">
 <meta content="" name="keywords">
 
